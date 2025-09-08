422e62a962ce027d62a39b1d45d31e21b25e1aa9
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2025-09-08 16:00:15 +0400

    feat: add empty message for empty search result

--- src/js/script.js
+++ src/js/script.js
@@ -41,6 +41,7 @@ function saveItemsToLocalStorage() {
 
 let todoItems = getItemsFromLocalStorage(localStorageKey);
 let filteredItems = null;
+let searchQuery = null;
 
 function renderCounters() {
   const allTasksNumber = todoItems.length;
@@ -123,6 +124,8 @@ function renderTasks() {
 
 function renderEmptyMessage() {
   const isEmptyItems = todoItems.length === 0;
+  const isEmptyFilteredItems = filteredItems && filteredItems.length === 0;
+
   emptyMessageElement.innerHTML = isEmptyItems
     ? `<svg
             width="56"
@@ -161,6 +164,44 @@ function renderEmptyMessage() {
             />
           </svg>
           <span>У вас пока нет задач</span>Создавайте и организуйте свои задачи`
+    : isEmptyFilteredItems
+    ? `<svg
+            width="56"
+            height="56"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16 4.00195C18.175 4.01406 19.3529 4.11051 20.1213 4.87889C21 5.75757 21 7.17179 21 10.0002V16.0002C21 18.8286 21 20.2429 20.1213 21.1215C19.2426 22.0002 17.8284 22.0002 15 22.0002H9C6.17157 22.0002 4.75736 22.0002 3.87868 21.1215C3 20.2429 3 18.8286 3 16.0002V10.0002C3 7.17179 3 5.75757 3.87868 4.87889C4.64706 4.11051 5.82497 4.01406 8 4.00195"
+              stroke="#0d0d0d"
+              stroke-width="1.5"
+            />
+            <path
+              d="M8 14H16"
+              stroke="#0d0d0d"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            />
+            <path
+              d="M7 10.5H17"
+              stroke="#0d0d0d"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            />
+            <path
+              d="M9 17.5H15"
+              stroke="#0d0d0d"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            />
+            <path
+              d="M8 3.5C8 2.67157 8.67157 2 9.5 2H14.5C15.3284 2 16 2.67157 16 3.5V4.5C16 5.32843 15.3284 6 14.5 6H9.5C8.67157 6 8 5.32843 8 4.5V3.5Z"
+              stroke="#0d0d0d"
+              stroke-width="1.5"
+            />
+          </svg>
+          <span>Задачи не найдены</span>Введите другое название задачи`
     : '';
 }
 
@@ -350,8 +391,8 @@ const onSearchFormSubmit = (event) => {
   event.preventDefault();
 };
 
-function filterTasks(value) {
-  const queryFormatted = value.toLowerCase();
+function filterTasks() {
+  const queryFormatted = searchQuery.toLowerCase();
 
   filteredItems = todoItems.filter(({ title }) => {
     const titleFormatted = title.toLowerCase();
@@ -359,19 +400,25 @@ function filterTasks(value) {
     return titleFormatted.includes(queryFormatted);
   });
 
-  renderTasks();
+  if (filteredItems.length === 0) {
+    renderEmptyMessage();
+    renderTasks();
+  } else {
+    renderTasks();
+  }
 }
 
 function resetFilter() {
   filteredItems = null;
+  renderEmptyMessage();
   renderTasks();
 }
 
 const onSearchInputChange = ({ target }) => {
-  const value = target.value.trim();
+  searchQuery = target.value.trim();
 
-  if (value.length > 0) {
-    filterTasks(value);
+  if (searchQuery.length > 0) {
+    filterTasks();
   } else {
     resetFilter();
   }
